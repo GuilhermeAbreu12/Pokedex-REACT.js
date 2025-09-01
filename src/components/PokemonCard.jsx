633dc367de420c2a607e9eaef7f82332aac452e7
GuilhermeAbreu12 <guilhermesabreu08@gmail.com>
2025-09-01 16:54:16 -0300

    feat: add styles

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,23 @@
+function PokemonCard({ pokemon }){
+    
+    const type = pokemon.types[0].type.name
+
+    return (
+        <div className={`card ${type}`}>
+            <div className='card-image-container'>
+                <img 
+                    src={pokemon.sprites.front_default} 
+                    alt={`Imagem do ${pokemon.name}`} 
+                    className='card-image'
+                />
+            </div>
+            <div className='card-info'>
+                <span className='card-number'>#{pokemon.id}</span>
+                <h3 className='card-name'>{pokemon.name}</h3>
+                <p className='card-type'>Type: {type}</p>
+            </div>
+        </div>
+    )
+}
+
+export default PokemonCard;
