cac18026c76130ed2b75bb7b223aa7effe433f68
GuilhermeAbreu12 <guilhermesabreu08@gmail.com>
2025-09-01 16:55:37 -0300

    feat: add pokedex system

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import PokemonCard from './components/PokemonCard';
 import './App.css'
 
 function App() {
+
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const response = await fetch(' https://pokeapi.co/api/v2/pokemon?limit=9') // Garante que todo carregamento precisa ser concluído para resposta
+      const data = await response.json()
+    
+      const detailedPokemon = await Promise.all(
+        data.results.map(async(pokemon) => {
+          const pokemonResponse = await fetch(pokemon.url);
+          return await pokemonResponse.json()
+        })
+      )
+
+      setPokemons(detailedPokemon)
+    };
+
+    fetchPokemons()
+  }, []);
+
   return (
     <>
-      
+      <div className='app-container'>
+        <h1>Pokedex</h1>
+        <div className='pokemon-grid'>
+          {pokemons.map(pokemons => (
+            <PokemonCard
+              key={pokemons.id}
+              pokemon={pokemons}
+            />
+          ))}
+        </div>
+      </div>
     </>
   )
 }
